222bd4e30aaadf080abd70ee378ca4bdb5762e98
henrikjacobsenfys <henrik.jacobsen.fys@gmail.com>
2025-01-27 11:19:29 +0100

    update __init__ of variable

--- src/easyscience/Objects/variable/__init__.py
+++ src/easyscience/Objects/variable/__init__.py
@@ -1,9 +1,11 @@
+from .descriptor_array import DescriptorArray
 from .descriptor_bool import DescriptorBool
 from .descriptor_number import DescriptorNumber
 from .descriptor_str import DescriptorStr
 from .parameter import Parameter
 
 __all__ = [
+    DescriptorArray,
     DescriptorBool,
     DescriptorNumber,
     DescriptorStr,
