997d5f9482d78d7809de3ac72cce893590ce5c60
Eric Lindgren <ericlin@chalmers.se>
2025-02-27 17:09:55 +0100

    add tests for abs and neg

--- tests/unit_tests/Objects/variable/test_descriptor_array.py
+++ tests/unit_tests/Objects/variable/test_descriptor_array.py
@@ -706,231 +706,51 @@ class TestDescriptorArray:
             function(descriptor_dimensionless, test)
         assert 'returned NotImplemented from' in str(e)
     
-    # def test_addition_with_array(self):
-    #     # When 
-    #     descriptor = DescriptorArray(name="name", value=1, variance=0.1)
+    def test_negation(self, descriptor):
+        # When 
+        # Then
+        result = -descriptor
 
-    #     # Then
-    #     result = descriptor + 1.0
-    #     result_reverse = 1.0 + descriptor
-
-    #     # Expect
-    #     assert type(result) == DescriptorArray
-    #     assert result.name == result.unique_name
-    #     assert result.value == 2.0
-    #     assert result.unit == "dimensionless"
-    #     assert result.variance == 0.1
-
-    #     assert type(result_reverse) == DescriptorArray
-    #     assert result_reverse.name == result_reverse.unique_name
-    #     assert result_reverse.value == 2.0
-    #     assert result_reverse.unit == "dimensionless"
-    #     assert result_reverse.variance == 0.1
-        
-    # @pytest.mark.parametrize("test, expected", [
-    #     (DescriptorArray("test", 2, "m", 0.01,),   DescriptorArray("test - name", 1, "m", 0.11)),
-    #     (DescriptorArray("test", 2, "cm", 0.01),   DescriptorArray("test - name", -98, "cm", 1000.01))],
-    #     ids=["regular", "unit_conversion"])
-    # def test_subtraction(self, descriptor: DescriptorArray, test, expected):
-    #     # When Then
-    #     result = test - descriptor
-
-    #     # Expect
-    #     assert type(result) == DescriptorArray
-    #     assert result.name == result.unique_name
-    #     assert result.value == expected.value
-    #     assert result.unit == expected.unit
-    #     assert result.variance == expected.variance
-
-    #     assert descriptor.unit == 'm'
-
-    # def test_subtraction_with_array(self):
-    #     # When 
-    #     descriptor = DescriptorArray(name="name", value=2, variance=0.1)
-
-    #     # Then
-    #     result = descriptor - 1.0
-    #     result_reverse = 1.0 - descriptor
-
-    #     # Expect
-    #     assert type(result) == DescriptorArray
-    #     assert result.name == result.unique_name
-    #     assert result.value == 1.0
-    #     assert result.unit == "dimensionless"
-    #     assert result.variance == 0.1
-
-    #     assert type(result_reverse) == DescriptorArray
-    #     assert result_reverse.name == result_reverse.unique_name
-    #     assert result_reverse.value == -1.0
-    #     assert result_reverse.unit == "dimensionless"
-    #     assert result_reverse.variance == 0.1
-
-    # @pytest.mark.parametrize("test", [1.0, DescriptorArray("test", 2, "s",)], ids=["sub_array_to_unit", "incompatible_units"])
-    # def test_subtraction_exception(self, descriptor: DescriptorArray, test):
-    #     # When Then Expect
-    #     with pytest.raises(UnitError):
-    #         result = test - descriptor
-    #     with pytest.raises(UnitError):
-    #         result_reverse = descriptor - test
-
-    # @pytest.mark.parametrize("test, expected", [
-    #     (DescriptorArray("test", 2, "m", 0.01,),   DescriptorArray("test * name", 2, "m^2", 0.41)),
-    #     (DescriptorArray("test", 2, "dm", 0.01),   DescriptorArray("test * name", 0.2, "m^2", 0.0041))],
-    #     ids=["regular", "base_unit_conversion"])
-    # def test_multiplication(self, descriptor: DescriptorArray, test, expected):
-    #     # When Then
-    #     result = test * descriptor
-
-    #     # Expect
-    #     assert type(result) == DescriptorArray
-    #     assert result.name == result.unique_name
-    #     assert result.value == expected.value
-    #     assert result.unit == expected.unit
-    #     assert result.variance == pytest.approx(expected.variance)
-
-    # def test_multiplication_with_array(self, descriptor: DescriptorArray):
-    #     # When Then
-    #     result = descriptor * 2.0
-    #     result_reverse = 2.0 * descriptor
-
-    #     # Expect
-    #     assert type(result) == DescriptorArray
-    #     assert result.name == result.unique_name
-    #     assert result.value == 2.0
-    #     assert result.unit == "m"
-    #     assert result.variance == 0.4
-
-    #     assert type(result_reverse) == DescriptorArray
-    #     assert result_reverse.name == result_reverse.unique_name
-    #     assert result_reverse.value == 2.0
-    #     assert result_reverse.unit == "m"
-    #     assert result_reverse.variance == 0.4
-
-    # @pytest.mark.parametrize("test, expected, expected_reverse", [
-    #     (DescriptorArray("test", 2, "m^2", 0.01,),   DescriptorArray("name / test", 0.5, "1/m", 0.025625), DescriptorArray("test / name", 2, "m", 0.41)),
-    #     (2, DescriptorArray("name / 2", 0.5, "m", 0.025), DescriptorArray("2 / name", 2, "1/m", 0.4))],
-    #     ids=["DescriptorArray", "scalar"])
-    # def test_division(self, descriptor: DescriptorArray, test, expected, expected_reverse):
-    #     # When Then
-    #     result = descriptor / test
-    #     result_reverse = test / descriptor
-
-    #     # Expect
-    #     assert type(result) == DescriptorArray
-    #     assert result.name == result.unique_name
-    #     assert result.value == expected.value
-    #     assert result.unit == expected.unit
-    #     assert result.variance == pytest.approx(expected.variance)
-
-    #     assert type(result_reverse) == DescriptorArray
-    #     assert result_reverse.name == result_reverse.unique_name
-    #     assert result_reverse.value == expected_reverse.value
-    #     assert result_reverse.unit == expected_reverse.unit
-    #     assert result_reverse.variance == pytest.approx(expected_reverse.variance)
-
-    # @pytest.mark.parametrize("test", [0, DescriptorArray("test", 0, "m", 0.01)], ids=["zero", "zero_descriptor"])
-    # def test_division_exception(self, descriptor: DescriptorArray, test):
-    #     # When Then Expect
-    #     with pytest.raises(ZeroDivisionError):
-    #         result = descriptor / test
-
-    # def test_division_exception_reverse(self):
-    #     # When 
-    #     descriptor = DescriptorArray(name="name", value=0, variance=0.1)
-
-    #     # Then Expect
-    #     with pytest.raises(ZeroDivisionError):
-    #         result = 2 / descriptor
-
-    # @pytest.mark.parametrize("test, expected", [
-    #     (DescriptorArray("test", 2), DescriptorArray("name ** test", 4, unit="m^2", variance=1.6)),
-    #     (2, DescriptorArray("name ** 2", 4, unit="m^2", variance=1.6)),
-    #     (-2, DescriptorArray("name ** -2", 0.25, unit="1/m^2", variance=0.00625))],
-    #     ids=["DescriptorArray", "scalar", "negative_array"])
-    # def test_power_of_descriptor(self, test, expected):
-    #     # When 
-    #     descriptor = DescriptorArray(name="name", value=2, unit="m", variance=0.1) 
-
-    #     # Then
-    #     result = descriptor ** test
-
-    #     # Expect
-    #     assert type(result) == DescriptorArray
-    #     assert result.name == result.unique_name
-    #     assert result.value == expected.value
-    #     assert result.unit == expected.unit
-    #     assert result.variance == expected.variance
-
-    # def test_power_of_dimensionless_descriptor(self):
-    #     # When 
-    #     descriptor = DescriptorArray(name="name", value=2, unit="dimensionless", variance=0.1) 
-
-    #     # Then
-    #     result = descriptor ** 0.5
-
-    #     # Expect
-    #     assert type(result) == DescriptorArray
-    #     assert result.name == result.unique_name
-    #     assert result.value == 1.4142135623730951
-    #     assert result.unit == "dimensionless"
-    #     assert result.variance == pytest.approx(0.0125)
-
-    # @pytest.mark.parametrize("descriptor, exponent, exception", [
-    #     (DescriptorArray("name", 2), DescriptorArray("test", 2, unit="m"), UnitError),
-    #     (DescriptorArray("name", 2), DescriptorArray("test", 2, variance=0.1), ValueError),
-    #     (DescriptorArray("name", 2, unit="m"), 0.5, UnitError),
-    #     (DescriptorArray("name", -2), 0.5, ValueError)],
-    #     ids=["descriptor_unit", "descriptor_variance", "fractional_of_unit", "fractonal_of_negative"])
-    # def test_power_of_descriptor_exceptions(self, descriptor, exponent, exception):
-    #     # When Then Expect
-    #     with pytest.raises(exception):
-    #         result = descriptor ** exponent
+        # Expect
+        expected = DescriptorArray(
+            name="name",
+            value=[[-1., -2.], [-3., -4.]],
+            unit="m",
+            variance=[[0.1, 0.2], [0.3, 0.4]],
+            description="description",
+            url="url",
+            display_name="display_name",
+            parent=None,
+        )
+        assert type(result) == DescriptorArray
+        assert result.name == result.unique_name
+        assert np.array_equal(result.value, expected.value)
+        assert result.unit == expected.unit
+        assert np.allclose(result.variance, expected.variance)
+        assert descriptor.unit == 'm'
 
 
-    # def test_descriptor_as_exponentiation(self):
-    #     # When 
-    #     descriptor = DescriptorArray(name="name", value=2) 
+    def test_abs(self, descriptor):
+        # When 
+        negated = DescriptorArray(
+            name="name",
+            value=[[-1., -2.], [-3., -4.]],
+            unit="m",
+            variance=[[0.1, 0.2], [0.3, 0.4]],
+            description="description",
+            url="url",
+            display_name="display_name",
+            parent=None,
+        )
 
-    #     # Then
-    #     result = 2 ** descriptor
+        # Then
+        result = abs(negated)
 
-    #     # Expect
-    #     assert result == 4
-
-    # @pytest.mark.parametrize("exponent, exception", [
-    #     (DescriptorArray("test", 2, unit="m"), UnitError),
-    #     (DescriptorArray("test", 2, variance=0.1), ValueError)],
-    #     ids=["descriptor_unit", "descriptor_variance"])
-    # def test_descriptor_as_exponentiation_exception(self, exponent, exception):
-    #     # When Then Expect
-    #     with pytest.raises(exception):
-    #         result = 2 ** exponent
-
-    # def test_negation(self):
-    #     # When 
-    #     descriptor = DescriptorArray(name="name", unit="m", value=2, variance=0.1) 
-
-    #     # Then
-    #     result = -descriptor
-
-    #     # Expect
-    #     assert type(result) == DescriptorArray
-    #     assert result.name == result.unique_name
-    #     assert result.value == -2
-    #     assert result.unit == "m"
-    #     assert result.variance == 0.1
-
-    # def test_abs(self):
-    #     # When 
-    #     descriptor = DescriptorArray(name="name", unit="m", value=-2, variance=0.1) 
-
-    #     # Then
-    #     result = abs(descriptor)
-
-    #     # Expect
-    #     assert type(result) == DescriptorArray
-    #     assert result.name == result.unique_name
-    #     assert result.value == 2
-    #     assert result.unit == "m"
-    #     assert result.variance == 0.1
+        # Expect
+        assert type(result) == DescriptorArray
+        assert result.name == result.unique_name
+        assert np.array_equal(result.value, descriptor.value)
+        assert result.unit == descriptor.unit
+        assert np.allclose(result.variance, descriptor.variance)
+        assert descriptor.unit == 'm'
 
